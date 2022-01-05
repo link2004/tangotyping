c44c975fb74cc8b7f3307139e66033541ca0c715
link2004 <riku2004@gmail.com>
2022-01-05 22:11:31 +0900

    vue-cliを実装,中身はほぼ何もしていない

---
+++ tango-typing/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div id="header">
+      <h1 id="title">Tango Typing</h1>
+      <div class="progress title-bar"></div>
+    </div>
+    <div id="body"><router-view/></div>
+    
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#title {
+  margin: 20px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+#body {
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ tango-typing/src/views/Typing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Typing">
+    <StartMenu title="タイトル"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import StartMenu from '@/components/startMenu.vue'
+
+export default {
+  name: 'Typing',
+  components: {
+    StartMenu
+  }
+}
+</script>
