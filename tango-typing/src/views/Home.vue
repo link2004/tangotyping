3393a78371dd184428c00cb165e20bae487355b7
link2004 <riku2004@gmail.com>
2023-03-08 17:24:48 +0900

    マイ単語帳をホームに追加

--- tango-typing/src/views/Home.vue
+++ tango-typing/src/views/Home.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="text-left">
     <div class="py-5 text-center">
-      <strong style="font-size:9vw">TangoTyping</strong>
+      <strong style="font-size: 9vw">TangoTyping</strong>
       <h2>単語帳タイピング</h2>
     </div>
 
     <div
       class="btn btn-info btn-block block-element mx-auto mb-3 shadow"
-      style="width:100%"
-      @click="$router.push({name:'mypage'})">マイ単語帳</div>
+      style="width: 100%"
+      @click="$router.push({ name: 'mypage' })"
+    >
+      マイ単語帳一覧
+    </div>
     <div
       class="btn btn-outline-primary btn-block block-element mx-auto mb-5 shadow"
-      style="width:100%"
-      @click="CreateNew">単語帳を作成</div>
-    
+      style="width: 100%"
+      @click="CreateNew"
+    >
+      単語帳を作成
+    </div>
 
     <!-- <div class="card mb-5 shadow">
       <div class="card-header"><h3>単語帳IDからプレイ</h3></div>
@@ -24,48 +29,91 @@
         </form>
       </div>
     </div> -->
-    
-    <div style="padding-bottom:10rem;">
-      <h2 class="text-left border-bottom mb-4">サンプル</h2>
-      <div class="btn btn-info btn-block block-element" @click=ClickedQuestion>Fruit</div>
+    <div v-if="user" style="padding-bottom: 3rem">
+      <h2 class="text-left border-bottom mb-4">マイ単語帳</h2>
+      <h3 v-if="isLoading">読み込み中...</h3>
+      <div style="display: flex; flex-wrap: wrap">
+        <div
+          v-for="(question, index) in questions"
+          :key="index"
+          class="btn btn-info btn-block block-element"
+          style="margin: 10px"
+          @click="ClickedQuestion(question.id)"
+        >
+          {{ question.title }}
+        </div>
+      </div>
     </div>
-    
-    
+
+    <div style="padding-bottom: 10rem">
+      <h2 class="text-left border-bottom mb-4">サンプル</h2>
+      <div
+        class="btn btn-info btn-block block-element"
+        @click="ClickedQuestion('10e36549-3657-46a0-b3be-776375e91044')"
+      >
+        Fruit
+      </div>
+    </div>
+
+    <div></div>
+
     <router-view></router-view>
   </div>
 </template>
 <script>
+import api from "../api.js";
 export default {
   data() {
     return {
-      inputQuestionID:null,
-    }
+      questions: [],
+      inputQuestionID: null,
+      isLoading: false,
+    };
   },
-  methods:{
-    ClickedQuestion: function(){
+  methods: {
+    ClickedQuestion: function(id){
       this.$router.push({
-        path:'m/typing/10e36549-3657-46a0-b3be-776375e91044',
+        path: `m/typing/`+id,
       })
     },
-    CreateNew: function(){
-      this.$router.push({name:'edit',params:{id:"new"}});
+    CreateNew: function () {
+      this.$router.push({ name: "edit", params: { id: "new" } });
     },
-    searchFromID: function(){
+    searchFromID: function () {
       this.$router.push({
-        path:`m/typing/${this.inputQuestionID}`,
-      })
-    }
+        path: `m/typing/${this.inputQuestionID}`,
+      });
+    },
+    queryQuestions: async function () {
+      //userIDから、問題をクエリ
+      var questions = [];
+      var items = await api.queryQuestion();
+      for (var item in items.questions) {
+        questions[item] = {
+          title: items.questions[item].title,
+          id: items.questions[item].id,
+        };
+      }
+      return questions;
+    },
   },
-  computed:{
-    user() {return this.$store.state.user}
-  }
-}
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  mounted: async function () {
+    this.isLoading = true;
+    this.questions = await this.queryQuestions();
+    this.isLoading = false;
+  },
+};
 </script>
 <style>
-.block-element{
-  font-size:2rem;
-  width:20rem;
-  border-radius:1rem;
-  padding:2rem;
+.block-element {
+  font-size: 2rem;
+  width: 20rem;
+  border-radius: 1rem;
+  padding: 2rem;
 }
 </style>
